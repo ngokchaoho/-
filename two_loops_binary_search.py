00fe2e121ba3a4847bcab2535793c47d7a5e7f17
ngokchaoho <ngokchao.ho@protonmail.com>
2021-06-04 01:02:53 +0800

    Format by autopep8

--- two_loops_binary_search.py
+++ two_loops_binary_search.py
@@ -1,25 +1,26 @@
 from bisect import bisect_left
-def binary_search(value,a):
+
+
+def binary_search(value, a):
     end = len(a)
-    index = bisect_left(a,value)
-    return index if index<end and a[index]==value else None
-    
-    
-    
-    
+    index = bisect_left(a, value)
+    return index if index < end and a[index] == value else None
+
+
 def solve():
     kk = []
     for c in range(n):
         for d in range(n):
-            kk.append(k[c]+k[d]) 
-            
+            kk.append(k[c]+k[d])
+
     for a in range(n):
         for b in range(n):
-                if binary_search(m-k[a]-k[b],kk):
-                    print('YES')
-                    return True
+            if binary_search(m-k[a]-k[b], kk):
+                print('YES')
+                return True
     print('NO')
     return False
+
 
 n = int(input())
 m = int(input())
